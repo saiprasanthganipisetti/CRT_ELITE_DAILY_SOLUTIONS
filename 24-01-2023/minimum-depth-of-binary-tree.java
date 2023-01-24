690fc35ad8f9138a5ea0bdd245d6d036a9ff1189
Ganipisetti Sai Prasanth <85571247+saiprasanthganipisetti@users.noreply.github.com>
2023-01-24 15:14:51 +0530

    Create minimum-depth-of-binary-tree.java

---
+++ 24-01-2023/minimum-depth-of-binary-tree.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        int l = 0, r = 0;
+        l += minDepth(root.left);
+        r += minDepth(root.right);
+        return l == 0 || r == 0 ? l + r + 1 : Math.min(l,r)+1;
+    }
+}
