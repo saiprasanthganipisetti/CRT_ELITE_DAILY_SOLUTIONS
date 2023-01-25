1c84f9a244cf8153844a12041c6ae0f3641f51d6
Ganipisetti Sai Prasanth <85571247+saiprasanthganipisetti@users.noreply.github.com>
2023-01-25 14:09:03 +0530

    Create balanced-binary-tree.java

---
+++ 25-01-2023/balanced-binary-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean ans = true;
+    int depth(TreeNode node){
+        
+        if(node == null)
+            return 0;
+        int ld = depth(node.left) + 1;
+        int rd = depth(node.right) + 1;
+        
+        
+        if(Math.abs(ld-rd)>1)
+            ans = false;
+        return Math.max(ld,rd);
+    }
+    public boolean isBalanced(TreeNode root) {
+        System.out.println(depth(root));
+        return ans;
+    }
+}
