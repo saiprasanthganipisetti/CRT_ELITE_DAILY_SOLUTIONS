4e2436382a74b2786c83828acfe4c2b20d0b2298
Ganipisetti Sai Prasanth <85571247+saiprasanthganipisetti@users.noreply.github.com>
2023-01-27 12:27:43 +0530

    Create level-order-traversal.py

---
+++ 27-01-2023/level-order-traversal.py
@@ -0,0 +1,22 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root == None:
+            return []
+        arr = []
+        arr.append([root.val])
+        print(arr)
+        temp = [root]
+        while len(temp):
+            n = len(temp)
+            a = []
+            for i in range(n):
+                curr = temp.pop(0)
+                if(curr.left != None):
+                    temp.append(curr.left)
+                    a.append(curr.left.val)
+                if(curr.right != None):
+                    temp.append(curr.right)
+                    a.append(curr.right.val)
+            if len(a) != 0:
+                arr.append(a)
+        return arr
