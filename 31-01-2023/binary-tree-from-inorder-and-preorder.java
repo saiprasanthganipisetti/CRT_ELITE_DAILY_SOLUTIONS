36e784c4e636063f23b606e86d415ca99070a29a
Ganipisetti Sai Prasanth <85571247+saiprasanthganipisetti@users.noreply.github.com>
2023-01-31 15:45:02 +0530

    Create binary-tree-from-inorder-and-preorder.java

---
+++ 31-01-2023/binary-tree-from-inorder-and-preorder.java
@@ -0,0 +1,20 @@
+class Solution {
+    int preindex = 0;
+    HashMap<Integer,Integer> map = new HashMap<>();
+    TreeNode create(int[] pre, int start,int end){
+        if(start > end)
+            return null;
+        int data = pre[preindex++];
+        TreeNode root = new TreeNode(data);
+        root.left = create(pre, start, map.get(data) - 1);
+        root.right = create(pre, map.get(data)+1, end);
+        return root;
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+        for(int i = 0; i < inorder.length; i++){
+            map.put(inorder[i], i);
+        }
+        return(create(preorder, 0, preorder.length - 1));
+    }
+}
