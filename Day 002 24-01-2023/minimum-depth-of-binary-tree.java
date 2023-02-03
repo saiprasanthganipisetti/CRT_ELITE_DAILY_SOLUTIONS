6967a73a7f71af59b66073d73fbd34c93c94d2f4
Ganipisetti Sai Prasanth <85571247+saiprasanthganipisetti@users.noreply.github.com>
2023-02-03 12:24:37 +0530

    Update minimum-depth-of-binary-tree.java

--- Day 002 24-01-2023/minimum-depth-of-binary-tree.java
+++ Day 002 24-01-2023/minimum-depth-of-binary-tree.java
@@ -5,6 +5,6 @@ class Solution {
         int l = 0, r = 0;
         l += minDepth(root.left);
         r += minDepth(root.right);
-        return l == 0 || r == 0 ? l + r + 1 : Math.min(l,r)+1;
+        return l == 0 || r == 0 ? l + r + 1 : Math.min(l,r)+ 1;
     }
 }
