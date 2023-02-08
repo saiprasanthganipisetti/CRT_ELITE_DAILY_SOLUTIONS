01305b0b8927be6f42550b43b7c5281c67e54276
Ganipisetti Sai Prasanth <85571247+saiprasanthganipisetti@users.noreply.github.com>
2023-02-08 16:36:57 +0530

    Create bfs.java

---
+++ Day 015 06-02-2023/bfs.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    void bfs(int s, ArrayList<ArrayList<Integer>> adj, boolean[] v, ArrayList<Integer> cat){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(s);
+        v[s] = true;
+        cat.add(s);
+        while(!q.isEmpty()){
+            int n = q.size();
+            
+            for(int i = 0; i < n; i++){
+                int aux = q.remove();
+                
+                for(int rel : adj.get(aux)){
+                    if(!v[rel]){
+                        q.add(rel);
+                        cat.add(rel);
+                        v[rel] = true;
+                    }
+                        
+                }
+            }
+        }
+    }
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+       
+        boolean[] v = new boolean[V];
+        ArrayList<Integer> cat = new ArrayList<>();
+        bfs(0, adj, v, cat);
+        return cat;
+    }
+}
